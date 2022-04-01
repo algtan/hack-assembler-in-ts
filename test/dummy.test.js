85bf28695255e71f990f5de313157d10b2add69c
Allen Tan <algtan.dev@gmail.com>
2022-03-31 19:45:05 -0700

    Update dependencies and add testing frameworks

---
+++ test/dummy.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('hack-assembler-in-ts', function () {
+  it("Doesn't have tests yet", function () {
+    expect(1 + 1).equals(2)
+  })
+})
